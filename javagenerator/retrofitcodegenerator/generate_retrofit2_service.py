00f4b2a730b5e3a8d51a7dbb61f299f94dc3f488
Pedro <pedrossgg1994@gmail.com>
2017-08-18 23:31:42 +0200

    add destination to retrofit2service

--- javagenerator/retrofitcodegenerator/generate_retrofit2_service.py
+++ javagenerator/retrofitcodegenerator/generate_retrofit2_service.py
@@ -1,5 +1,6 @@
 import os
 import json
+import re
 
 class GenerateRetrofit2Service:
     def createRetrofit2Service(self, moduleName, packageName, listRequestJson):
@@ -8,7 +9,7 @@ class GenerateRetrofit2Service:
         file.write("package " + str(packageName) + ";\n"
     +"\n"
     +"\n"
-    + self.createImports(listRequestJson)
+    + self.createImports()
     +"public interface Retrofit2Service {\n"
     +"\n"
     + self.createRequests(listRequestJson)
@@ -16,20 +17,26 @@ class GenerateRetrofit2Service:
 
     def createRequests(self, listRequestJson):
         stringRequest = ""
-        cont = 0
         for i in listRequestJson:
             jsonEncoded = json.loads(i.replace("\n", ""))
-            stringRequest += "@" + jsonEncoded["method"]
+            url = jsonEncoded["url"]
+            regularExpresion = '\/(.*)\?'
+            try:
+                destiny = re.search(regularExpresion, url, re.I | re.U).group(0)
+            except AttributeError:
+                destiny = str(url)[str(url).index('/'):]
+            destiny = destiny.replace("/", "").replace("?", "")
+            stringRequest += "@" + jsonEncoded["method"] + \
+                             "(\"" + destiny + "\")"
             stringRequest += "\n"
-            stringRequest += "Call<Object> " + "method" + str(cont) + "("
-            cont += 1
+            stringRequest += "Call<Object> " + destiny + "("
             # add headers to requests
             for h in jsonEncoded["headers"]:
                 stringRequest += "@Header(\"" + str(h["key"]) + "\") String " + str(h["key"]) + ","
             # add querys to requests
             for q in jsonEncoded["querys"]:
                 stringRequest += "@Query(\"" + str(q["key"]) + "\") String " + str(q["key"]) + ","
-            stringRequest += ");\n"
+            stringRequest += ");\n\n"
             # delete last , to fix format code
         return stringRequest.replace(",)", ")")
 
--- pedro/src/main/java/com/example/library/Retrofit2Service.java
+++ pedro/src/main/java/com/example/library/Retrofit2Service.java
@@ -6,38 +6,55 @@ import retrofit2.http.*;
 
 public interface Retrofit2Service {
 
-@DELETE
-Call<Object> method0(@Header("Credentials-AccessToken") String credentials-accesstoken,@Query("path") String path);
-@POST
-Call<Object> method1(@Header("Credentials-AccessToken") String credentials-accesstoken);
-@POST
-Call<Object> method2();
-@GET
-Call<Object> method3(@Header("Credentials-AccessToken") String credentials-accesstoken,@Query("path") String path);
-@GET
-Call<Object> method4(@Header("Credentials-AccessToken") String credentials-accesstoken,@Query("key") String key);
-@GET
-Call<Object> method5(@Header("Credentials-AccessToken") String credentials-accesstoken,@Query("perPage") String perpage,@Query("page") String page,@Query("orderBy") String orderby,@Query("order") String order,@Query("search") String search,@Query("path") String path);
-@DELETE
-Call<Object> method6(@Header("Credentials-AccessToken") String credentials-accesstoken,@Query("path") String path);
-@POST
-Call<Object> method7(@Header("Credentials-AccessToken") String credentials-accesstoken,@Query("path") String path);
-@GET
-Call<Object> method8(@Header("Credentials-AccessToken") String credentials-accesstoken,@Query("key") String key);
-@DELETE
-Call<Object> method9(@Header("Credentials-AccessToken") String credentials-accesstoken);
-@DELETE
-Call<Object> method10(@Header("invited") String invited);
-@POST
-Call<Object> method11(@Header("Credentials-AccessToken") String credentials-accesstoken);
-@POST
-Call<Object> method12(@Header("Credentials-AccessToken") String credentials-accesstoken,@Query("path") String path);
-@POST
-Call<Object> method13(@Header("Credentials-AccessToken") String credentials-accesstoken);
-@POST
-Call<Object> method14(@Header("Credentials-AccessToken") String credentials-accesstoken,@Header("invited") String invited);
-@GET
-Call<Object> method15(@Header("Credentials-AccessToken") String credentials-accesstoken,@Query("path") String path);
-@POST
-Call<Object> method16(@Header("Credentials-AccessToken") String credentials-accesstoken,@Query("to") String to);
+@DELETE("deleteFile")
+Call<Object> deleteFile(@Header("Credentials-AccessToken") String Credentials-AccessToken,@Query("path") String path);
+
+@POST("shareFiles")
+Call<Object> shareFiles(@Header("Credentials-AccessToken") String Credentials-AccessToken);
+
+@POST("increaseCount")
+Call<Object> increaseCount();
+
+@GET("getMedia")
+Call<Object> getMedia(@Header("Credentials-AccessToken") String Credentials-AccessToken,@Query("path") String path);
+
+@GET("getPublicFIle")
+Call<Object> getPublicFIle(@Header("Credentials-AccessToken") String Credentials-AccessToken,@Query("key") String key);
+
+@GET("filesList")
+Call<Object> filesList(@Header("Credentials-AccessToken") String Credentials-AccessToken,@Query("perPage") String perPage,@Query("page") String page,@Query("orderBy") String orderBy,@Query("order") String order,@Query("search") String search,@Query("path") String path);
+
+@DELETE("deleteAll")
+Call<Object> deleteAll(@Header("Credentials-AccessToken") String Credentials-AccessToken,@Query("path") String path);
+
+@POST("addFile")
+Call<Object> addFile(@Header("Credentials-AccessToken") String Credentials-AccessToken,@Query("path") String path);
+
+@GET("getPublicMedia")
+Call<Object> getPublicMedia(@Header("Credentials-AccessToken") String Credentials-AccessToken,@Query("key") String key);
+
+@DELETE("deletePublicKey")
+Call<Object> deletePublicKey(@Header("Credentials-AccessToken") String Credentials-AccessToken);
+
+@DELETE("deleteAccess")
+Call<Object> deleteAccess(@Header("invited") String invited);
+
+@POST("refreshPublicKey")
+Call<Object> refreshPublicKey(@Header("Credentials-AccessToken") String Credentials-AccessToken);
+
+@POST("addDIr")
+Call<Object> addDIr(@Header("Credentials-AccessToken") String Credentials-AccessToken,@Query("path") String path);
+
+@POST("createPublicKey")
+Call<Object> createPublicKey(@Header("Credentials-AccessToken") String Credentials-AccessToken);
+
+@POST("newAccess")
+Call<Object> newAccess(@Header("Credentials-AccessToken") String Credentials-AccessToken,@Header("invited") String invited);
+
+@GET("getFile")
+Call<Object> getFile(@Header("Credentials-AccessToken") String Credentials-AccessToken,@Query("path") String path);
+
+@POST("sendData")
+Call<Object> sendData(@Header("Credentials-AccessToken") String Credentials-AccessToken,@Query("to") String to);
+
 }
